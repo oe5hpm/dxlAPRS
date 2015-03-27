7009160f4337afcb72878425dc0d03b8af0fe9d0
Hannes Petermaier <oe5hpm@oevsv.at>
2015-03-27 07:08:38 +0100

    udphub: several changes to routing behaviour
    see documentation (dxlAPRS_common/NEW.txt) for further details.
    
    +  "Via Digi(s) gehoert" wird nicht mehr gespeichert.
    +  Via Digis senden routet auch mit via Calls wenn diese ohne h-bit sind
    +  -a peer-to-peer routing ein (geht auch ohne -u host)
    +  -L Timer fuer Routing zu allen anderen ssid vom User.
    +  Wenn der User via mehreren ip/port bekannt ist, gehts zu allen seinen
    +  ip/ports bis eine Anwort von genau dem ssid kommt zu dem die Daten gehen.
    +  Nach dieser Zeit (seit zuletzt irgend ein ssid gehoert) routets nur zu
    +  den bekannten ssid, -L 0 ist off
    
    Signed-off-by: Hannes Petermaier <oe5hpm@oevsv.at>

--- src/udphub.c
+++ src/udphub.c
@@ -84,14 +84,19 @@ static unsigned char CRCL[256];
 
 static unsigned char CRCH[256];
 
+static char peertopeer;
+
 static char show;
 
 static char checkdigiip;
 
 static pUSER users;
 
+static unsigned long alllifetime;
+
 static unsigned long systime;
 
+/* time for all/unknown ssid */
 static unsigned long lifetime;
 
 static unsigned long touserport;
@@ -257,6 +262,17 @@ static char GetNum(const char h[], unsigned long h_len, unsigned long * n)
    return h[i]==0;
 } /* end GetNum() */
 
+/*
+PROCEDURE Inittable(fn:ARRAY OF CHAR);
+VAR fd:INTEGER;
+BEGIN
+  fd:=OpenRead(fn);
+  IF fd<0 THEN Err("-f: file not found") END;  
+
+    
+
+END Inittable;
+*/
 
 static void parms(void)
 {
@@ -274,7 +290,18 @@ static void parms(void)
             if (!GetNum(h, 1024ul, &i)) Err("-l minutes", 11ul);
             lifetime = i*60UL;
          }
+         else if (h[1U]=='L') {
+            Lib_NextArg(h, 1024ul);
+            if (!GetNum(h, 1024ul, &i)) Err("-L minutes", 11ul);
+            alllifetime = i*60UL;
+         }
+         else if (h[1U]=='a') peertopeer = 1;
          else if (h[1U]=='m') {
+            /*
+                  ELSIF h[1]="f" THEN                                       (* init filename *)
+                    NextArg(h);
+                    Inittable(h);
+            */
             Lib_NextArg(h, 1024ul);
             if (!GetNum(h, 1024ul, &maxentries)) Err("-m number", 10ul);
          }
@@ -293,6 +320,12 @@ static void parms(void)
          else {
             if (h[1U]=='h') {
                osi_WrLn();
+               osi_WrStrLn(" -a                                route user-to-\
+host and user-to-user", 71ul);
+               osi_WrStrLn(" -L <time>                         minutes route \
+to all ssid\'s (default 10 min)", 80ul);
+               osi_WrStrLn("                                   0 no all ssid \
+routing", 57ul);
                osi_WrStrLn(" -l <time>                         minutes lifeti\
 me (default 1 week)", 69ul);
                osi_WrStrLn(" -m <maxentries>                   else delete ol\
@@ -300,7 +333,7 @@ d entries (default 1000)", 74ul);
                osi_WrStrLn(" -p <userport>                     udp port for u\
 sers", 54ul);
                osi_WrStrLn(" -u <x.x.x.x:destport:listenport>  axudp to digi \
-/listenport no check ip", 73ul);
+/listenport check ip", 70ul);
                osi_WrStrLn(" -v                                verbous",
                 43ul);
                osi_WrStrLn(" -h                                this", 40ul);
@@ -366,14 +399,14 @@ static void showframe(const char s[], unsigned long s_len, const char b[],
 
 
 static char cmpcall(const RAWCALL c, const char b[], unsigned long b_len,
-                unsigned long start)
+                unsigned long start, char withssid)
 {
    unsigned long i;
    for (i = 0UL; i<=5UL; i++) {
       if (c[i]!=b[i+start]) return 0;
    } /* end for */
-   return ((unsigned long)(unsigned char)c[6U]/2UL&15UL)==((unsigned long)
-                (unsigned char)b[start+6UL]/2UL&15UL);
+   return !withssid || ((unsigned long)(unsigned char)c[6U]/2UL&15UL)
+                ==((unsigned long)(unsigned char)b[start+6UL]/2UL&15UL);
 } /* end cmpcall() */
 
 
@@ -428,10 +461,13 @@ static void AddIp(unsigned long ip, unsigned long dp, const char buf[],
    /* for fast find, rechain to first position*/
    struct USER * anonym;
    struct USER * anonym0;
+   if (!((unsigned long)(unsigned char)buf[13UL]&1) && (unsigned char)
+                buf[20UL]>=(unsigned char)'\200') return;
+   /* via digi, store only direct heard */
    u = users;
    last = 0;
    while (u) {
-      if (cmpcall(u->call, buf, buf_len, 7UL)) {
+      if (cmpcall(u->call, buf, buf_len, 7UL, 1)) {
          if (last) {
             last->next = u->next;
             u->next = users;
@@ -486,14 +522,15 @@ static void AddIp(unsigned long ip, unsigned long dp, const char buf[],
 } /* end AddIp() */
 
 
-static char FindIp(unsigned long * toip0, unsigned long * toport,
-                const char buf[], unsigned long buf_len)
+static char FindIp(unsigned long * toip, unsigned long * toport,
+                const char buf[], unsigned long buf_len,
+                unsigned long callpos)
 {
    pUSER u;
    u = users;
    while (u) {
-      if (cmpcall(u->call, buf, buf_len, 0UL)) {
-         *toip0 = u->uip;
+      if (cmpcall(u->call, buf, buf_len, callpos, 1)) {
+         *toip = u->uip;
          *toport = u->dport;
          return 1;
       }
@@ -502,6 +539,82 @@ static char FindIp(unsigned long * toip0, unsigned long * toport,
    return 0;
 } /* end FindIp() */
 
+
+static char FindNextSsid(pUSER * u, unsigned long * toip,
+                unsigned long * toport, const char buf[],
+                unsigned long buf_len, unsigned long callpos)
+{
+   if (*u==0) *u = users;
+   else *u = (*u)->next;
+   while (*u) {
+      if ((*u)->htime+alllifetime>=systime && cmpcall((*u)->call, buf,
+                buf_len, callpos, 0)) {
+         *toip = (*u)->uip;
+         *toport = (*u)->dport;
+         return 1;
+      }
+      *u = (*u)->next;
+   }
+   return 0;
+} /* end FindNextSsid() */
+
+
+static char FindsameIP(pUSER un, unsigned long toip, unsigned long toport,
+                const char buf[], unsigned long buf_len,
+                unsigned long callpos)
+{
+   pUSER u;
+   u = users;
+   while (u!=un) {
+      if (((u->htime+alllifetime>=systime && cmpcall(u->call, buf, buf_len,
+                callpos, 0)) && toip==u->uip) && toport==u->dport) return 1;
+      u = u->next;
+   }
+   return 0;
+} /* end FindsameIP() */
+
+
+static char sendtouser(char ubuf0[], unsigned long ubuf_len, long blen0)
+{
+   unsigned long toip;
+   unsigned long userdport0;
+   pUSER u;
+   long ci;
+   long res0;
+   char ok0;
+   ok0 = 0;
+   ci = 0L;
+   do {
+      /*WrInt(ci, 5); WrInt(ORD(ubuf[ci]), 5);
+                WrInt(ORD(ubuf[ci+(CALLLEN-1)]), 5); WrStrLn("<<<"); */
+      if (ci==0L || (unsigned char)ubuf0[ci+6L]<(unsigned char)'\200') {
+         /* dest or via call with no h-bit */
+         if (FindIp(&toip, &userdport0, ubuf0, ubuf_len, (unsigned long)ci)) {
+            /* user and ssid fits */
+            res0 = udpsend(usersock, ubuf0, blen0, userdport0, toip);
+            ok0 = 1;
+         }
+         else if (alllifetime>0UL) {
+            /* look for fitting user with any ssid */
+            u = 0;
+            while (FindNextSsid(&u, &toip, &userdport0, ubuf0, ubuf_len,
+                (unsigned long)ci)) {
+               if (!(ok0 && FindsameIP(u, toip, userdport0, ubuf0, ubuf_len,
+                (unsigned long)ci))) {
+                  /* have not sent to same ip:port before */
+                  res0 = udpsend(usersock, ubuf0, blen0, userdport0, toip);
+                  ok0 = 1;
+               }
+            }
+         }
+      }
+      if (ci==0L) ci = 14L;
+      else ci += 7L;
+   } while (!((ci>63L || ci+7L>=blen0) || ((unsigned long)(unsigned char)ubuf0[ci-1L]&1)));
+   /* address field end */
+   return ok0;
+} /* end sendtouser() */
+
 static char ubuf[338];
 
 static long blen;
@@ -509,8 +622,6 @@ static long blen;
 static long res;
 
 static unsigned long fromip;
-
-static unsigned long toip;
 
 static unsigned long userdport;
 
@@ -527,13 +638,15 @@ extern int main(int argc, char **argv)
    Storage_BEGIN();
    Gencrctab();
    show = 0;
+   peertopeer = 0;
    touserport = 0UL;
    digisock = -1L;
    maxentries = 1000UL;
    lifetime = 604800UL;
-   parms();
-   if (digisock<0L) Err("need -u parameter", 18ul);
+   alllifetime = 600UL;
    users = 0;
+   parms();
+   if (!peertopeer && digisock<0L) Err("need -u parameter", 18ul);
    usersock = openudp();
    if ((touserport==0UL || usersock<0L) || bindudp(usersock, touserport)<0L) {
       Err("cannot bind userport (-p userport)", 35ul);
@@ -541,20 +654,19 @@ extern int main(int argc, char **argv)
    systime = TimeConv_time();
    for (;;) {
       fdclr();
-      fdsetr((unsigned long)digisock);
+      if (digisock>=0L) fdsetr((unsigned long)digisock);
       fdsetr((unsigned long)usersock);
       if (selectrw(10UL, 0UL)>0L) {
-         if (issetr((unsigned long)digisock)) {
+         if (digisock>=0L && issetr((unsigned long)digisock)) {
             /* data from digi */
             fromip = digiip;
             blen = getudp(digisock, ubuf, 338ul, &fromip, &userdport,
                 checkdigiip);
             if (blen>=17L) {
                if (show) showframe("from digi ", 11ul, ubuf, 338ul);
-               if (FindIp(&toip, &userdport, ubuf, 338ul)) {
-                  res = udpsend(usersock, ubuf, blen, userdport, toip);
+               if (!sendtouser(ubuf, 338ul, blen) && show) {
+                  osi_WrStrLn(" digi out user not found", 25ul);
                }
-               else if (show) osi_WrStrLn(" digi out user not found", 25ul);
             }
          }
          if (issetr((unsigned long)usersock)) {
@@ -567,7 +679,12 @@ extern int main(int argc, char **argv)
                   showframe(" from user ", 12ul, ubuf, 338ul);
                }
                AddIp(fromip, userdport, ubuf, 338ul);
-               res = udpsend(digisock, ubuf, blen, todigiport, digiip);
+               if (digisock>=0L) {
+                  res = udpsend(digisock, ubuf, blen, todigiport, digiip);
+               }
+               if ((peertopeer && !sendtouser(ubuf, 338ul, blen)) && show) {
+                  osi_WrStrLn(" peer-to-peer no user found", 28ul);
+               }
             }
          }
       }
