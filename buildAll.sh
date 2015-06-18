a1eba71871df42f6fdb3669193a2c556a2b83b15
Hannes Schmelzer <oe5hpm@oevsv.at>
2015-06-18 21:05:38 +0200

    build: adapt buildAll.sh for new makefiles
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- buildAll.sh
+++ buildAll.sh
@@ -5,7 +5,14 @@ PLATTFORMS="x86 armv6 armv7hf"
 for i in $PLATTFORMS; do
 	cd src
 	make clean
-	make $i
+
+	if [ "$i"=="armv6" ]; then
+		CROSS_COMPILE=/opt/rpi-tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi- make $i
+	elif [ "$i"=="armv7hf" ]; then
+		CROSS_COMPILE=/opt/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- make $i
+	else
+		make $i
+	fi
 	cd ..
 	./packCurrent.sh $i
 done
