18b7b475672eb3d4c346bf2cc98ce780d2d29da3
Hannes Schmelzer <oe5hpm@oevsv.at>
2015-06-18 21:21:32 +0200

    build: fix buildAll script
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- buildAll.sh
+++ buildAll.sh
@@ -6,15 +6,19 @@ for i in $PLATTFORMS; do
 	cd src
 	make clean
 
-	if [ "$i"=="armv6" ]; then
+	case $i in
+	"armv6")
+		echo "---------------armv6------------- ($i) "
 		CROSS_COMPILE=/opt/rpi-tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/bin/arm-bcm2708hardfp-linux-gnueabi- make $i
-	elif [ "$i"=="armv7hf" ]; then
+	;;
+	"armv7hf")
 		CROSS_COMPILE=/opt/gcc-linaro-4.9-2014.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- make $i
-	else
+	;;
+	"x86")
+		echo "----------------x86---------------"
 		make $i
-	fi
+	;;
+	esac
 	cd ..
 	./packCurrent.sh $i
 done
-
-
