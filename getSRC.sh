e80185192726bcf7670381daa9557662a49766bf
Hannes Schmelzer <oe5hpm@oevsv.at>
2016-02-24 21:55:25 +0100

    use xds-header files from libxds and drop them from sourcetree
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- getSRC.sh
+++ getSRC.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+EXCL="FileSys.h IOChan.h InOut.h ProgEnv.h RealIO.h RealMath.h StdChans.h Storage.h SysClock.h TimeConv.h X2C.h xPOSIX.h xmRTS.h xrcArythmetics.h"
 git branch -D fromDXL
 git checkout master
 git checkout -b fromDXL
@@ -8,6 +9,7 @@ cd src
 wget http://oe5dxl.ampr.at/aprs/intel-debug.tgz || exit 1
 tar -xzf intel-debug.tgz
 rm intel-debug.tgz
+rm $EXCL
 git checkout -- keysym.h
 git checkout -- pngread.c
 cd ..
