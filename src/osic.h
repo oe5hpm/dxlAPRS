ab8182a8fea4230b23524bb6decbc46856124657
Hannes Schmelzer <oe5hpm@oevsv.at>
2016-02-28 20:51:34 +0100

    xds_rem: adapt osic.h to corresponing osic.c
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- src/osic.h
+++ src/osic.h
@@ -16,15 +16,27 @@
 
 void osic_WrLn(void);
 
+void osic_WerrLn(void);
+
 void osic_WrStr(char s[], unsigned long s_len);
 
 void osic_WrStrLn(char s[], unsigned long s_len);
+
+void osic_WerrStr(char s[], unsigned long s_len);
+
+void osic_WerrStrLn(char s[], unsigned long s_len);
 
 void osic_WrUINT32(uint32_t x, unsigned long witdh);
 
 void osic_WrFixed(float x, long place, unsigned long witdh);
 
 void osic_WrHex(unsigned long n, unsigned long f);
+
+int getptsname(int fd, char *name, int len);
+
+int grantpts(int fd);
+
+int unlockpts(int fd);
 
 long osic_OpenAppendLong(char fn[], unsigned long fn_len);
 
@@ -47,15 +59,15 @@ void osic_Close(long fd);
 void osic_CloseSock(long fd);
 
 long osic_RdBin(long fd,
-		      char buf[], unsigned long buf_len,
-		      unsigned long size);
+		char buf[], unsigned long buf_len,
+		unsigned long size);
 
 void osic_WrBin(long fd, char buf[],
-		      unsigned long buf_len,
-		      unsigned long size);
+		unsigned long buf_len,
+		unsigned long size);
 
 void osic_Rename(char fname[], unsigned long fname_len,
-		       char newname[], unsigned long newname_len);
+		 char newname[], unsigned long newname_len);
 
 int osic_Size(int fd);
 
@@ -63,8 +75,36 @@ void osic_Seek(long fd, unsigned long pos);
 
 void osic_Seekcur(long fd, long rel);
 
+void osic_Remove(X2C_CHAR fname[], X2C_CARD32 fname_len, X2C_BOOLEAN *done);
+
+X2C_BOOLEAN osic_Exists(X2C_CHAR fname[], X2C_CARD32 fname_len);
+
+int osic_symblink(char *existing, char *newname);
+
+void osic_NextArg(char s[], unsigned long s_len);
+
+void osic_Fill(X2C_ADDRESS buf, unsigned long len, char byte);
+
+double osic_Random(void);
+
 void X2C_BEGIN(int *argc, char *argv[],
 	       int gc_auto, long gc_threshold, long heap_limit);
+
+void X2C_PCOPY(void **ppcpy, size_t size);
+
+void X2C_PFREE(void *p);
+
+X2C_INT32 X2C_TRUNCI(X2C_LONGREAL x, X2C_INT32 min0, X2C_INT32 max0);
+
+X2C_CARD32 X2C_TRUNCC(X2C_LONGREAL x, X2C_CARD32 min0, X2C_CARD32 max0);
+
+X2C_REAL X2C_DIVR_F(X2C_REAL a, X2C_REAL b);
+
+X2C_LONGREAL X2C_DIVL_F(X2C_LONGREAL a, X2C_LONGREAL b);
+
+X2C_pVOID X2C_COPY(void *src, size_t src_len, void *dst, size_t dst_len);
+
+void X2C_ABORT(void);
 
 X2C_REAL RealMath_cos(X2C_REAL x);
 
@@ -82,10 +122,42 @@ X2C_REAL RealMath_exp(X2C_REAL x);
 
 X2C_REAL RealMath_power(X2C_REAL base, X2C_REAL exponent);
 
-X2C_CARD32 TimeConv_time(void);
+void X2C_EXIT(void);
+
+void X2C_HALT(X2C_INT32 x);
 
 void Storage_ALLOCATE(X2C_ADDRESS *p, X2C_CARD32 size);
 
 void Storage_DEALLOCATE(X2C_ADDRESS *a, X2C_CARD32 size);
+
+X2C_CARD32 TimeConv_time(void);
+
+X2C_CARD32 X2C_LSH(X2C_CARD32 a, X2C_INT16 length, X2C_INT32 n);
+
+X2C_BOOLEAN X2C_IN(X2C_CARD32 i, X2C_CARD16 bits, X2C_CARD32 set);
+
+X2C_BOOLEAN X2C_INL(X2C_CARD32 i, X2C_CARD16 bits, LSET set);
+
+X2C_INT32 X2C_MOD_F(X2C_INT32 a, X2C_INT32 b);
+
+X2C_CHAR X2C_CAP(X2C_CHAR x);
+
+LSET X2C_INCL(LSET set, X2C_CARD32 i, X2C_CARD16 bits);
+
+LSET X2C_COMPLEMENT(LSET res, LSET a, X2C_CARD16 length);
+
+void X2C_DYNALLOCATE(X2C_ADDRESS *a, size_t size, size_t lens[], size_t dims);
+
+void X2C_DYNDEALLOCATE(X2C_ADDRESS *a);
+
+int X2C_STRCMP_PROC(X2C_pVOID x, size_t alen, X2C_pVOID y, size_t blen);
+
+X2C_INT32 X2C_QUO_F(X2C_INT32 a, X2C_INT32 b);
+
+X2C_LONGREAL X2C_EXPRI(X2C_LONGREAL base, X2C_INT32 ex);
+
+X2C_PROC X2C_CHKPROC_F(X2C_PROC p);
+
+X2C_pVOID X2C_CHKNIL_F(X2C_pVOID p);
 
 #endif /* __OSIC_H__ */
