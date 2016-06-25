854dbf0c39adfbab9486fe5d2e04b0b80d0cb4d2
Hannes Schmelzer <oe5hpm@oevsv.at>
2016-06-25 12:19:30 +0200

    osic: fix X2C_EXCL function
    the invert of mask was not formed correctly.
    
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- src/osic.c
+++ src/osic.c
@@ -446,7 +446,7 @@ void X2C_EXCL(unsigned long *set, unsigned long i, unsigned long bits)
 {
 	if (i >= bits)
 		assert(0);
-	set[(int)i / X2C_LSET_SIZE] &= ~1L << ((int)i % X2C_LSET_SIZE);
+	set[(int)i / X2C_LSET_SIZE] &= ~(1L << ((int)i % X2C_LSET_SIZE));
 }
 
 unsigned long *X2C_COMPLEMENT(unsigned long *res, unsigned long *a, unsigned long length)
