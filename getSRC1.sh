9b385b5f13db2d65eeaaf5dcbfd3b116b8c9b36c
Hannes Schmelzer <oe5hpm@oevsv.at>
2016-06-28 16:32:24 +0200

    build: update getSRC1.sh
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- getSRC1.sh
+++ getSRC1.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#EXCL="FileSys.h IOChan.h InOut.h ProgEnv.h RealIO.h RealMath.h StdChans.h Storage.h SysClock.h TimeConv.h xPOSIX.h xmRTS.h xrcArythmetics.h"
+EXCL="X.h Xlib.h Xutil.h cursorfont.h keysym.h"
 
 ./getNEWtxt.sh
 cd src
 wget http://oe5dxl.ampr.at/aprs/intel-debug.tgz || exit 1
 tar -xzf intel-debug.tgz
 rm intel-debug.tgz
-#rm $EXCL
-git checkout -- keysym.h
+rm $EXCL
 git checkout -- pngread.c
 cd ..
 ./license-add.sh
+git checkout -- src/xosi.c
 git status
