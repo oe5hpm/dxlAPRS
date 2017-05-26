cfb69110f5d80c50aef4eccb80a349e35006da64
Hannes Schmelzer <oe5hpm@oevsv.at>
2017-05-26 11:38:03 +0200

    bring getSRC1.sh up to date
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- getSRC1.sh
+++ getSRC1.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-EXCL="X.h Xlib.h Xutil.h cursorfont.h keysym.h"
+EXCL="X.h Xlib.h Xutil.h cursorfont.h keysym.h jconfig.h jmorecfg.h jpeglib.h png.h"
 
 ./getNEWtxt.sh
 cd src
 wget http://oe5dxl.ampr.at/aprs/intel-debug.tgz || exit 1
 tar -xzf intel-debug.tgz
-rm intel-debug.tgz
+mv intel-debug.tgz ../
 rm $EXCL
+rm -r -f lib_x86
+rm -r -f lib_x86_64
 git checkout -- pngread.c
+git checokut -- osic.c
 cd ..
 ./license-add.sh
-git checkout -- src/xosi.c
 git status
