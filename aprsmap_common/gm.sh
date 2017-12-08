2a42230da6781097169f31d6feb817c198c9042f
Thomas de Lellis <t52ta6ek@yahoo.com>
2017-12-08 20:18:41 +0100

    gm.sh now references an external map list file

--- aprsmap_common/gm.sh
+++ aprsmap_common/gm.sh
@@ -3,136 +3,91 @@
 # file created by aprsmap of a list of tiles to fetch
 ORIGFN="gettiles"
 
+# filename of default list of maps
+MAPFN="maplist"
+
 # map directory path specified by aprsmap
-#DIR="osm"
 DIR=$2
 
-# move the original file to a temporary file to prevent a race
-# condition with aprsmap where aprsmap sometimes attempts to write
-# to the original file before this script completes processing.
+# move the original file to a temporary file to prevent a race condition where
+# aprsmap seems to attempt to continue writing to the original file before
+# this script completes processing
 FN=$(mktemp)
+#mv $ORIGFN $FN
 cp $ORIGFN $FN
 
-# 3 sec timeout, 1 retry
-WGETCMDBASE="wget -q -T 3 -t 1 "
+# use user-specified maplist if passed via command line, else use default
+MAPLIST=$3
+if [ -z $3 ] ; then
+    MAPLIST=$MAPFN
+fi
 
-OSMSERVERS="\
-             http://a.tile.openstreetmap.org \
-             http://b.tile.openstreetmap.org \
-             http://c.tile.openstreetmap.org \
-             http://osm.oe2xzr.ampr.at/osm/tiles \
-            "
+# 5 sec timeout
+WGETCMDBASE="wget -q -T 5 "
 
-OSMSERVERSTOPO="\
-             http://a.tile.thunderforest.com/landscape \
-             http://b.tile.thunderforest.com/landscape \
-             http://c.tile.thunderforest.com/landscape \
-             http://osm.oe2xzr.ampr.at/osm/tiles_topo \
-            "
-
-OSMSERVERSCYCLE="\
-             http://a.tile.opencyclemap.org/cycle \
-             http://b.tile.opencyclemap.org/cycle \
-             http://osm.oe2xzr.ampr.at/osm/tiles_cyclemap \
-            "
-
-OSMSERVERSTRANSPORT="\
-             http://a.tile.opencyclemap.org/transport \
-             http://b.tile.opencyclemap.org/transport \
-            "                                         
-
-# note: mapquest sat and quest tile servers are no longer providing direct access
-OSMSERVERSSAT="\
-             http://otile1.mqcdn.com/tiles/1.0.0/sat \
-             http://otile2.mqcdn.com/tiles/1.0.0/sat \
-             http://otile3.mqcdn.com/tiles/1.0.0/sat \
-             http://otile4.mqcdn.com/tiles/1.0.0/sat \
-             http://osm.oe2xzr.ampr.at/osm/tiles_sat \
-            "
-
-OSMSERVERSQUEST="\
-             http://otile1.mqcdn.com/tiles/1.0.0/osm \
-             http://otile2.mqcdn.com/tiles/1.0.0/osm \
-             http://otile3.mqcdn.com/tiles/1.0.0/osm \
-             http://otile4.mqcdn.com/tiles/1.0.0/osm \
-            "                                         
-
-#--------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 # if the tiles directory does not exist then create one
-if ! [ -d $DIR ]; then
+if ! [ -d $DIR ] ; then
     mkdir $DIR
 fi
 
-# if tiles directory found
+# if tiles directory exists and the tile list is accessible
 if [ -d $DIR ] ; then
-    for i in 1 2 3 4 5 6 7 8 9 10; do
+    if [ -r $FN ] ; then
+# create the rest of the map tile directory structure if necessary
+        while read mapname mapz mapx mapy ; do
+            if ! [ -d $DIR/$mapname ] ; then
+                mkdir $DIR/$mapname
+            fi
+            if ! [ -d $DIR/$mapname/$mapz ] ; then
+                mkdir $DIR/$mapname/$mapz
+            fi
+            if ! [ -d $DIR/$mapname/$mapz/$mapx ] ; then
+                mkdir $DIR/$mapname/$mapz/$mapx
+            fi
 
-	if [ -r $FN ] ; then
-	    while read  mapa mapb mapc mapd ; do
-        	if ! [ -d $DIR/$mapa ] ; then
-		    mkdir $DIR/$mapa
-        	fi
-        	if ! [ -d $DIR/$mapa/$mapb ] ; then
-		    mkdir $DIR/$mapa/$mapb
-        	fi
-        	if ! [ -d $DIR/$mapa/$mapb/$mapc ] ; then
-		    mkdir $DIR/$mapa/$mapb/$mapc
-        	fi
-		
-		if [ tiles = $mapa ]; then
-		    SERVERS=$OSMSERVERS
-		    EXTENT="png"
-		    
-		elif [ tiles_topo = $mapa ]; then
-		    SERVERS=$OSMSERVERSTOPO
-		    EXTENT="png"
-		    
-		elif [ tiles_cyclemap = $mapa ]; then
-		    SERVERS=$OSMSERVERSCYCLE                                 
-		    EXTENT="png"
-		    
-		elif [ tiles_transport = $mapa ]; then
-		    SERVERS=$OSMSERVERSTRANSPORT
-		    EXTENT="png"
-
-# note: mapquest sat and quest tile servers are no longer providing direct access
-		elif [ tiles_sat = $mapa ]; then
-		    SERVERS=$OSMSERVERSSAT
-		    EXTENT="jpg"
-		    
-		elif [ tiles_quest = $mapa ]; then
-		    SERVERS=$OSMSERVERSQUEST
-		    EXTENT="png"
-		fi
-		
-		MAPFILE=$DIR/$mapa/$mapb/$mapc/$mapd.$EXTENT
-		echo "Loading tile: $MAPFILE ..."
-		for OSMSERVER in $SERVERS ; do
-		    echo -n "-> from $OSMSERVER - "
-		    $WGETCMDBASE -O $MAPFILE "$OSMSERVER/$mapb/$mapc/$mapd.png"
-		    if [ -s $MAPFILE ] ; then
-			echo "done."
-			if [ jpg = $EXTENT ]; then
-			    jpegtopnm $MAPFILE | pnmtopng > $DIR/$mapa/$mapb/$mapc/$mapd.png
-			    rm -f $MAPFILE
-			fi
-			break
-		    else
-		    	rm -f $MAPFILE
-			echo "Unsuccessful getting tile, trying next mirror."
+# try to match the requested map tile with a server in the map list file
+	    if [ -r $MAPLIST ] ; then
+		while read tileid tileformat tileurl tileapikey tilecomment ; do
+		    if [ $tileid = $mapname ] ; then
+			SERVER=$tileurl
+			EXTENT=$tileformat
+			break;
 		    fi
-		done
-	    done < $FN
-	    rm -f $FN
-	fi
-    done
+		done < $MAPLIST
+		if [ -z $SERVER ] ; then
+		    echo "Unable to find map named \"$mapname\" in map file: $MAPLIST"
+		    exit 1;
+		fi
+	    else
+		echo "Unable to access map file: $MAPLIST"
+		exit 1
+	    fi
+
+# if an api key or access token is present
+	    if [ $(echo $tileapikey | cut -c1-8) = "?apikey=" ] ||
+	       [ $(echo $tileapikey | cut -c1-14) = "?access_token=" ] ; then
+		APIKEY=$tileapikey
+	    fi
+
+#------------------------------------------------------------------------------
+
+# so far so good, then initiate the retrieval of all tiles
+            MAPFILE=$mapz/$mapx/$mapy.$EXTENT
+            echo "Loading tile: $MAPFILE ..."
+            echo -n "-> from $SERVER - "
+            $WGETCMDBASE -O $DIR/$mapname/$MAPFILE $SERVER/$MAPFILE$APIKEY &
+            echo "initiated download."
+        done < $FN
+        rm -f $FN
+    else
+	echo "Unable to access tiles file: $FN"
+	exit 1
+    fi
 else
-    echo Directory $DIR not found.
-    rm -f $ORIGFN
+    echo "Directory $DIR not found."
     exit 1
 fi
-
-rm -f $ORIGFN
 
 exit 0
