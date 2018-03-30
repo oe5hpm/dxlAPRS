e4f192c1f42dbcd15ad906515036764386d32630
Hannes Schmelzer <oe5hpm@oevsv.at>
2018-03-30 22:17:27 +0200

    refactor getSRC1.sh
    Signed-off-by: Hannes Schmelzer <oe5hpm@oevsv.at>

--- getSRC1.sh
+++ getSRC1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 EXCL="X.h Xlib.h Xutil.h cursorfont.h keysym.h jconfig.h jmorecfg.h jpeglib.h png.h"
-
+IGNL="pngread.c osic.c rsc.h pastewrapper.c pastewrapper.h ../aprsmap_common/kiss-th-d72.txt ../aprsmap_common/kiss9k6-th-d72.txt ../aprsmap_common/kisson.txt"
 ./getNEWtxt.sh
 cd src
 wget http://oe5dxl.ampr.at/aprs/intel-debug.tgz || exit 1
@@ -9,8 +9,11 @@ mv intel-debug.tgz ../
 rm $EXCL
 rm -r -f lib_x86
 rm -r -f lib_x86_64
-git checkout -- pngread.c
-git checkout -- osic.c
+for i in $IGNL; do
+	echo ignoring changes of $i ...
+	git checkout -- $i
+done
+
 cd ..
 ./license-add.sh
 git status
